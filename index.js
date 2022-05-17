d4d15b0d6265e97ab90a34446605b2bc8a16e6a6
CaraErskine <caraerskine@gmail.com>
2022-05-17 19:18:08 -0400

    commit tree 2805973f52384c6671a2e321a40ea679b06968b0

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
-}
+  let result 
+  if (someValue <= 400) {
+  result = someValue <= 400;
+  return 'This one is on me!' }
+  
+  else if (someValue >= 400, someValue <2000) {
+  result = someValue >= 400 , someValue <2000;
+  return 'That will be twenty bucks.' }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  else if (someValue >2000, someValue <2500) {
+  result = someValue >2000, someValue <2500
+  return 'I will gladly take your thirty bucks.'}  
 
-function switchOnCharmFromTip(){
+  else if (someValue >2500 ) {
+  result = someValue >2500;
+  return 'No can do.' }
+  }
+
+  function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
   // Write your code here!
+  
+function switchOnCharmFromTip(greeting){
+  switch(greeting) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+// Write your code here!
 }
